aa4197a9acf547eea8d9f96ff63825e5706c4c25
Danieujooj <ggg4m3r@gmail.com>
2019-06-26 05:04:09 +0900

    save

--- www/components/js/acao.js
+++ www/components/js/acao.js
@@ -34,10 +34,40 @@ function scanBarcode() {
 }
 
 //Mapa
-$(document).on('click','#mapa',function(){
-    function geolocationSuccess (position) {
+  
+  window.onload = function(){
 
-        navigator.notification.alert('Latitude: ' + position.coords.latitude + '\n' + 'Longitude: '+ position.coords.longitude);
-    };
-    navigator.geolocation.getCurrentPosition(geolocationSuccess);
-});
+    //configurando plugin de notificações
+    document.addEventListener("deviceready", onDeviceReady, false);
+    function onDeviceReady() {
+        console.log(navigator.notification);
+    }
+
+    //configurando plugin de geolocalização
+    document.addEventListener("deviceready", onDeviceReady, false);
+    function onDeviceReady() {
+      console.log("navigator.geolocation works well");
+    }
+
+      var onSuccess = function(position) {
+
+        L.mapquest.key = 'VuATH4WRuwVc1ZJUa2GyU3oOaDiljlOr';
+
+        var map = L.mapquest.map('map', {
+        center: [position.coords.latitude, position.coords.longitude],//Configurando coordenadas recebidas pelo gps
+        layers: L.mapquest.tileLayer('map'),
+        zoom: 12
+        });
+
+        map.addControl(L.mapquest.control());
+      };
+
+      //função de erro
+      function onError(error){
+        navigator.notification.alert('code: '    + error.code    + '\n' +
+                                    'message: ' + error.message + '\n');
+      }
+
+      //Pegando coordenadas atuais
+      navigator.geolocation.getCurrentPosition(onSuccess, onError);
+    }
